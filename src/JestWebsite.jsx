c3dccfda41a18543722f56e97404a6b1a2c2253b
Tim Seckinger <seckinger.tim@gmail.com>
2019-09-11 16:32:37 +0200

    Jest Website slide

---
+++ src/JestWebsite.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useDeck } from 'mdx-deck';
+
+import jestWebsiteFallback from '!!file-loader!../img/jest-website.png';
+
+const JestWebsite = () => {
+  const { step, setState } = useDeck();
+  return (
+    <>
+      {step ? (
+        <img
+          alt="Jest website screenshot"
+          src={jestWebsiteFallback}
+          style={{ height: '90%' }}
+        />
+      ) : (
+        <iframe
+          title="Jest Website"
+          src="https://jestjs.io/"
+          style={{
+            width: '95%',
+            height: '95%',
+            border: '1px solid rgb(194, 19, 37)',
+          }}
+        />
+      )}
+      <button
+        onClick={() => setState({ step: step ? 0 : 1 })}
+        style={{ color: 'gray' }}
+      >
+        Fallback
+      </button>
+    </>
+  );
+};
+
+export default JestWebsite;
